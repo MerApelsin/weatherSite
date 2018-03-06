00fd64edddb3b67db1dcb1909ba3d3bf76497a85
MerApelsin <cecilia.carlsson93@gmail.com>
2018-03-06 14:31:55 +0100

    meep
    all dem console.logs

--- weather.js
+++ weather.js
@@ -1,17 +1,22 @@
 getData();
-function testing(){
-  let testTime = 1509993277;
+timeTesting();
+function timeTesting(){
+  //Hämta nuvarande tid
   let timeTransfer = new Date();
   timeTransfer.getTime;
-  console.log(timeTransfer);
+  let stringTime = timeTransfer.toTimeString();
+  let stringDate = formatDate(timeTransfer);
+  // console.log(<fetchname>.timeSeries[x].validTime); - return timeSeries
+  //the data is valid for both date & actual time - YYYY-DD-MMTHH:SS:MMZ
+  console.log(stringTime);
+  console.log(stringDate);
 }
 
 async function getData()
 {
-  //Stockholm hårdkodat fornow
+  //Stockholm hårdkodat for-now
   let response = await fetchData('https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.068581/lat/59.329323/data.json');
-  //printData(response);
-  console.log(response);
+  handleData(response);
 }
 
 async function fetchData(url)
@@ -21,25 +26,35 @@ async function fetchData(url)
 			return data;
 		}
 
-function printData(apiResponse){
+//ska ha med - aktuell plats, temp - parameter 't' i timeSeries,
+//datum + tid - validTime @ timeSeries
+function handleData(apiResponse){
   let theDiv = document.getElementById("today");
   let myTextTag = document.createElement('p');
-  //time fix
-  //let currentTime = new Date();
-  //currentTime.setTime(apiResponse.currently.time);
-
-  let textNode = document.createTextNode("current temperature: " + apiResponse.currently.temperature);
-
+  //time - to be here
+  //hämta temperaturen - console.log(apiResponse.timeSeries[x].parameters[1].values); -para[1] = alltid sant
+  let textNode = document.createTextNode("current temperature: " + apiResponse.timeSeries[1].parameters[1].values + " Celsius");
+  console.log("Pure API response - time series");
+  console.log(apiResponse.timeSeries);
   myTextTag.appendChild(textNode);
   theDiv.appendChild(myTextTag);
 }
 
-//TO BE PROGED
-function iconDecide(apiIconResponse){
-  let img = "";
-  if(apiIconResponse.currently.icon == "rain"){
-    img = "img\rain.png"
-  }
+//TO BE PROGED - bestämma vilken vädericon som ska visas
+function iconDecide(iconResponse){
+  //switch-cases?
+}
 
-  return img;
+//Formatera dagens datum till en YYYY-MM-DD sträng
+//för att kunna jämföra @ api respons
+function formatDate(date) {
+    var d = new Date(date);
+    month = '' + (d.getMonth() + 1);
+    day = '' + d.getDate();
+    year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
 }

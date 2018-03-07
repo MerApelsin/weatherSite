be9fb5f6be418ecd86ce22b6d2e2c6f66a9a6692
MerApelsin <cecilia.carlsson93@gmail.com>
2018-03-07 12:51:53 +0100

    borken if cases

--- weather.js
+++ weather.js
@@ -1,15 +1,29 @@
 getData();
-timeTesting();
+//timeTesting();
 function timeTesting(){
-  //Hämta nuvarande tid
+  /*//Hämta nuvarande tid
   let timeTransfer = new Date();
-  timeTransfer.getTime;
   let stringTime = timeTransfer.toTimeString();
   let stringDate = formatDate(timeTransfer);
   // console.log(<fetchname>.timeSeries[x].validTime); - return timeSeries
-  //the data is valid for both date & actual time - YYYY-DD-MMTHH:SS:MMZ
-  console.log(stringTime);
-  console.log(stringDate);
+  //the data is valid for both date & actual time - UTC - YYYY-DD-MMTHH:SS:MMZ
+  console.log(timeTransfer);
+  console.log(timeTransfer.toISOString().slice(0, 19) + 'Z'); //UTC*/
+  let myTime = new Date;
+  let meep = myTime.getTime();
+  console.log(meep);
+  let myTimeDate = myTime.slice(0,10);
+  let myTimeHours = myTime.slice(11,16);
+  console.log("myTime: " + myTime);
+  console.log("myTimeDate: " + myTimeDate);
+  console.log("myTimeHours: " + myTimeHours);
+  console.log(myTime);
+  let testString = "13:23:12";
+  let testString2 = "04:00:00";
+  let testInt = testString.split(':').map(Number);
+  let testInt2 = testString2.split(':').map(Number);
+  console.log(testInt, testInt2);
+
 }
 
 async function getData()
@@ -33,28 +47,91 @@ function handleData(apiResponse){
   let myTextTag = document.createElement('p');
   //time - to be here
   //hämta temperaturen - console.log(apiResponse.timeSeries[x].parameters[1].values); -para[1] = alltid sant
-  let textNode = document.createTextNode("current temperature: " + apiResponse.timeSeries[1].parameters[1].values + " Celsius");
+  let myTime = getDateString();
+  let myTimeDate = myTime.slice(0,10);
+  let myTimeHours = myTime.slice(11,16);
+  let thisValue = 0;
+  for(let i = 0; i < apiResponse.timeSeries.length; i++)
+  {
+    //<fetchname>.timeSeries[x].validTime
+    let responseTime = apiResponse.timeSeries[i].validTime
+    let rTimeDate = responseTime.slice(0,10);
+    let rTimeHours = responseTime.slice(11,16);
+    console.log("rtimehrs: " + rTimeHours);
+    console.log("mytime: " + myTimeHours);
+    //let isRightTime = compareTimeArray(myTimeHours,rTimeHours);
+    if(rTimeDate == myTimeDate && compareTimeArray(myTimeHours,rTimeHours) == 1)
+    {
+      console.log("round:  ");
+      console.log(i);
+      console.log("rTimeDate: " + rTimeDate);
+      console.log("rTimeHours: " + rTimeHours);
+      thisValue = i;
+      break;
+    }
+  }
+  let textNode = document.createTextNode("current temperature: " + apiResponse.timeSeries[thisValue].parameters[1].values + " Celsius");
   console.log("Pure API response - time series");
   console.log(apiResponse.timeSeries);
+  console.log("validTime: ");
+  console.log(apiResponse.timeSeries[thisValue].validTime);
+  console.log("thisValue: ");
+  console.log(thisValue);
   myTextTag.appendChild(textNode);
   theDiv.appendChild(myTextTag);
 }
 
 //TO BE PROGED - bestämma vilken vädericon som ska visas
-function iconDecide(iconResponse){
+function iconDecide(iconResponse)
+{
   //switch-cases?
 }
 
-//Formatera dagens datum till en YYYY-MM-DD sträng
-//för att kunna jämföra @ api respons
-function formatDate(date) {
-    var d = new Date(date);
-    month = '' + (d.getMonth() + 1);
-    day = '' + d.getDate();
-    year = d.getFullYear();
+//Hämta dagens datum + tid, konventera till ISO sträng för att
+//jämföra med validTime @ api.
+function getDateString()
+{
+  let timeTransfer = new Date();
+  //console.log(timeTransfer);
+  let dateString = timeTransfer.toISOString().slice(0, 19) + 'Z';
 
-    if (month.length < 2) month = '0' + month;
-    if (day.length < 2) day = '0' + day;
+  //YYYY-MM-DDTHH:SS:MMZ
+  return dateString;
+}
 
-    return [year, month, day].join('-');
+//Ta emot 2 stängar med 'int', gör om dessa till int array för att jämföra
+//returnerar true eller false
+function compareTimeArray(myHours,apiHours)
+{
+  let myHoursArray = myHours.split(':').map(Number);
+  let apiHoursArray = apiHours.split(':').map(Number);
+  myHoursArray[0] = myHoursArray[0]+1;
+  let compare = 0;
+
+  if(myHoursArray[0] > apiHoursArray[0])
+  {
+    console.log("first if");
+    compare = -1;
+    //return false;
+  }
+  else if (myHoursArray[0] == apiHours[0] && myHoursArray[1] > 30)
+  {
+    console.log("second if");
+    compare = -1;
+    //return false;
+  }
+  else if (myHoursArray[0] == apiHoursArray[0] && myHoursArray[1] < 30)
+  {
+    console.log("third if");
+    compare = 1;
+    //return true;
+  }
+  else if(myHoursArray[0] < apiHoursArray[0] && myHoursArray[1] < 30)
+  {
+    console.log("fourth if");
+    compare = 1;
+    //return true;
+  }
+  console.log(compare);
+  return compare;
 }

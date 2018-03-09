caae5342e8e042e3efb9f366cc7406f4264502de
MerApelsin <cecilia.carlsson93@gmail.com>
2018-03-09 09:45:41 +0100

    Cleaned up!
    Rensat och "förfinat" js:en.

--- weather.js
+++ weather.js
@@ -1,14 +1,7 @@
-//getData();
+getData();
 //timeTesting();
 function timeTesting(){
-  /*//Hämta nuvarande tid
-  let timeTransfer = new Date();
-  let stringTime = timeTransfer.toTimeString();
-  let stringDate = formatDate(timeTransfer);
-  // console.log(<fetchname>.timeSeries[x].validTime); - return timeSeries
-  //the data is valid for both date & actual time - UTC - YYYY-DD-MMTHH:SS:MMZ
-  console.log(timeTransfer);
-  console.log(timeTransfer.toISOString().slice(0, 19) + 'Z'); //UTC*/
+
   let myTime = getDateString();
   let myTimeDate = myTime.slice(0,10);
   let myTimeArray = myTimeDate.split("-");
@@ -46,42 +39,42 @@ async function fetchData(url)
 			return data;
 		}
 
-//ska ha med - aktuell plats, temp - parameter 't' i timeSeries,
-//datum + tid - validTime @ timeSeries
+//hantera api-svaret
 function handleData(apiResponse){
   handleToday(apiResponse);
   handleWeek(apiResponse);
 }
 
+//Skriver ut dagens prognos för den aktuella tiden.
+
 function handleToday(apiResponse)
 {
   let theDiv = document.getElementById("today");
   let myTextTag = document.createElement('p');
-  //time - to be here
-  //hämta temperaturen - console.log(apiResponse.timeSeries[x].parameters[1].values); -para[1] = alltid sant
+
+//Gör mitt datum + min tid till en sträng för att splita och kunna jämföra korrekt
   let myTime = getDateString();
   let myTimeDate = myTime.slice(0,10);
   let myTimeHours = myTime.slice(11,16);
   let thisValue = 0;
 
+  //Loopar igenom för att hitta rätt plats
   for(let i = 0; i < apiResponse.timeSeries.length; i++)
   {
-    //<fetchname>.timeSeries[x].validTime
+    //Hömtar ut vilken tid prognosen är relevant för, splittar till array för jämföring
     let responseTime = apiResponse.timeSeries[i].validTime;
     let rTimeDate = responseTime.slice(0,10);
     let rTimeHours = responseTime.slice(11,16);
-    console.log(apiResponse.timeSeries);
 
-    //let isRightTime = compareTimeArray(myTimeHours,rTimeHours);
-    if(rTimeDate === myTimeDate && compareTimeArray(myTimeHours,rTimeHours) == true)
+    if(rTimeDate === myTimeDate && compareTime(myTimeHours,rTimeHours) == true)
     {
       thisValue = i;
       break;
     }
   }
   document.getElementById("showDate").innerHTML = "Visar prognos för Idag ("+myTimeDate+"), klockan: " + getTimeFetched(apiResponse.timeSeries[thisValue].validTime);
-  let thisTempPara = findTemp(apiResponse.timeSeries[thisValue].parameters, false);
-  let thisIconPara = findTemp(apiResponse.timeSeries[thisValue].parameters, true);
+  let thisTempPara = findTemp(apiResponse.timeSeries[thisValue].parameters);
+  let thisIconPara = findIcon(apiResponse.timeSeries[thisValue].parameters);
   iconDraw(apiResponse.timeSeries[thisValue].parameters[thisIconPara].values[0],"todaysIcon","iconText");
   let textNode = document.createTextNode("Nuvarande temperatur är ungefär: " + apiResponse.timeSeries[thisValue].parameters[thisTempPara].values[0] + " Celsius");
   myTextTag.appendChild(textNode);
@@ -90,49 +83,43 @@ function handleToday(apiResponse)
 
 function handleWeek(apiResponse)
 {
-  let weekToShow = getWeekArray();
+  let week = getWeek();
   let wantTime = "12:00:00";
-  //weekToShow innehåller 5 element - 0->4, med framtida datum som önskas
-  //tiden ska alltid vara 12:00 för framtida dagar.
-  let valueArray = [];
-  let temperArray = [];
-  let iconArray = [];
-  for(let i = 0; i < weekToShow.length; i++)
+
+  //tiden ska alltid vara 12:00 UTC för framtida dagar.
+  let theseValues = [];
+  let temperatures = [];
+  let icons = [];
+  //for-loop för att finna alla värden som behövs för vekoprognos
+  for(let i = 0; i < week.length; i++)
   {
+    //for-loop för att hitta vart i svaret de relevanta datumen/tiderna finns
     for(let a = 0; a < apiResponse.timeSeries.length; a++)
     {
       let responseTime = apiResponse.timeSeries[a].validTime;
       let rTimeDate = responseTime.slice(0,10);
       let rTimeHours = responseTime.slice(11,16);
-      if(rTimeDate === weekToShow[i] && compareTimeArray(wantTime,rTimeHours) == true)
+      if(rTimeDate === week[i] && compareTime(wantTime,rTimeHours) == true)
       {
-        valueArray.push(a); //borde bli 5
+        theseValues.push(a);
         break;
       }
     }
-    console.log("valueArray");
-    console.log(valueArray);
-    temperArray.push(findTemp(apiResponse.timeSeries[valueArray[i]].parameters, false));
-    iconArray.push(findTemp(apiResponse.timeSeries[valueArray[i]].parameters, true));
+    temperatures.push(findTemp(apiResponse.timeSeries[theseValues[i]].parameters));
+    icons.push(findIcon(apiResponse.timeSeries[theseValues[i]].parameters));
   }
-  drawWeek(apiResponse,valueArray,temperArray,iconArray,weekToShow)
+  drawWeek(apiResponse,theseValues,temperatures,icons,week);
 }
 
-function drawWeek(apiResponse,valueArray,temperArray,iconArray, weekToShow)
+function drawWeek(apiResponse,theseValues,temperatures,icons,week)
 {
-  //apiResponse.timeSeries[thisValue].parameters[thisTempPara].values[0]
-  //iconDraw(apiResponse.timeSeries[thisValue].parameters[thisIconPara].values[0],iconId,textId);
-  console.log(apiResponse.timeSeries);
-    for(let a = 0; a < weekToShow.length; a++)
+    for(let a = 0; a < week.length; a++)
     {
       let prognosTime = document.getElementById("date"+a);
       let temperatur = document.getElementById("temp"+a);
-      let tempToWrite = apiResponse.timeSeries[valueArray[a]].parameters[temperArray[a]].values[0];
-      console.log("tempToWrite");
-      console.log(tempToWrite);
-      console.log();
-      prognosTime.innerHTML = weekToShow[a];
-      iconDraw(apiResponse.timeSeries[valueArray[a]].parameters[iconArray[a]].values[0],"future"+a,"ftext"+a);
+      let tempToWrite = apiResponse.timeSeries[theseValues[a]].parameters[temperatures[a]].values[0];
+      prognosTime.innerHTML = week[a];
+      iconDraw(apiResponse.timeSeries[theseValues[a]].parameters[icons[a]].values[0],"future"+a,"ftext"+a);
       temperatur.innerHTML = "Beräknas vara: " +tempToWrite + " grader";
     }
 }
@@ -142,44 +129,8 @@ function iconDraw(iconValue,iconId,textId)
 {
   iconPlace = document.getElementById(iconId);
   iconText = document.getElementById(textId);
-  //todaysIcon.src = "img/<weather>.png";
+
   switch (iconValue) {
-    case 27:
-    case 26:
-    case 25:
-    case 17:
-    case 16:
-    case 15:
-    case 14:
-    //Snöfall av någon typ
-      iconPlace.src = "img/snow.png";
-      iconText.innerHTML = "Snöfall av någon typ.";
-      break;
-    case 24:
-    case 23:
-    case 22:
-    case 12:
-    case 13:
-    //snöblandat regn av någon typ
-      iconPlace.src = "img/snownrain.png";
-      iconText.innerHTML = "Snöblandat regn av någon typ.";
-      break;
-    case 21:
-    case 11:
-    //åska
-      iconPlace.src = "img/thunder.png";
-      iconText.innerHTML = "Åskväder av någon typ.";
-      break;
-    case 20:
-    case 19:
-    case 18:
-    case 10:
-    case 9:
-    case 8:
-    //regn av något slag
-      iconPlace.src = "img/rain.png";
-      iconText.innerHTML = "Regn av någon typ.";
-      break;
     case 1:
     case 2:
     //klart
@@ -199,11 +150,47 @@ function iconDraw(iconValue,iconId,textId)
       iconPlace.src = "img/cloudy.png";
       iconText.innerHTML = "Molningt eller dimmigt.";
       break;
+    case 8:
+    case 9:
+    case 10:
+    case 18:
+    case 19:
+    case 20:
+    //regn av något slag
+      iconPlace.src = "img/rain.png";
+      iconText.innerHTML = "Regn av någon typ.";
+      break;
+    case 14:
+    case 15:
+    case 15:
+    case 17:
+    case 25:
+    case 26:
+    case 27:
+    //Snöfall av någon typ
+      iconPlace.src = "img/snow.png";
+      iconText.innerHTML = "Snöfall av någon typ.";
+      break;
+    case 12:
+    case 13:
+    case 22:
+    case 23:
+    case 24:
+    //snöblandat regn av någon typ
+      iconPlace.src = "img/snownrain.png";
+      iconText.innerHTML = "Snöblandat regn av någon typ.";
+      break;
+    case 11:
+    case 21:
+    //åska
+      iconPlace.src = "img/thunder.png";
+      iconText.innerHTML = "Åskväder av någon typ.";
+      break;
   }
 }
 
 //Hämta dagens datum + tid, konventera till ISO sträng för att
-//jämföra med validTime @ api.
+//jämföra med validTime @ api. Konventerar automatiskt till UTC.
 function getDateString()
 {
   let timeTransfer = new Date();
@@ -214,18 +201,11 @@ function getDateString()
   return dateString;
 }
 
-function findTemp(thisTimeSeries, iconBool)
+//Kollar igenom den relevanta parametern och söker efter namnet
+//som är relevant för resp. function - temperaturen & icon
+function findIcon(thisTimeSeries)
 {
   for (let a = 0; a < thisTimeSeries.length; a++)
-  {
-    if(iconBool == false)
-    {
-      if(thisTimeSeries[a].name == "t" && thisTimeSeries[a].unit == "Cel")
-      {
-        return a;
-      }
-    }
-    else if(iconBool == true)
     {
         if(thisTimeSeries[a].name == "Wsymb2")
         {
@@ -233,11 +213,21 @@ function findTemp(thisTimeSeries, iconBool)
         }
     }
   }
+
+function findTemp(thisTimeSeries)
+{
+  for (let a = 0; a < thisTimeSeries.length; a++)
+  {
+    if(thisTimeSeries[a].name == "t" && thisTimeSeries[a].unit == "Cel")
+    {
+      return a;
+    }
+  }
 }
 
 //Ta emot 2 stängar med 'int', gör om dessa till int array för att jämföra
 //returnerar true eller false
-function compareTimeArray(myHours,apiHours)
+function compareTime(myHours,apiHours)
 {
   //[HH]:[MM]:[SS]
   let myTime = myHours.split(':').map(Number);
@@ -259,36 +249,38 @@ function compareTimeArray(myHours,apiHours)
   }
 }
 
+//Fixar sträng för utskrift på sidan
 function getTimeFetched(validTimeString)
 {
   let hoursString = validTimeString.slice(11,16);
   let timeArray = hoursString.split(':');
   timeArray[0] = parseInt(timeArray[0]);
   timeArray[0] = timeArray[0]+1;
-  console.log(timeArray);
   return string = timeArray.join(":");
-
 }
 
-function getWeekArray()
+//Skapar en array med de datum som är 5 dagar framåt från nu
+function getWeek()
 {
   let myTime = getDateString();
   let myTimeDate = myTime.slice(0,10);
   let myTimeArray = myTimeDate.split("-");
-  let timeUseArray = [];
+  let useDates = [];
 
   for(let i = 0; i < 5; i++)
   {
     myTimeArray[2] = parseInt(myTimeArray[2]);
     myTimeArray[2] = myTimeArray[2]+1;
+    //Då dessa ska jämföras med en sträng är det viktigt
+    //att 0:an finns med om det är under tvåsiffrig.
     if(myTimeArray[2] < 10)
     {
       let temp = myTimeArray[2].toString();
       myTimeArray[2] = "0" + temp;
     }
     let tempString = myTimeArray.join("-");
-    timeUseArray.push(tempString);
+    useDates.push(tempString);
   }
 
-  return timeUseArray;
+  return useDates;
 }
